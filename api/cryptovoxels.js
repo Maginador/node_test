f4a0c0cb4214035f0651f6e66ea808cbc734f325
Maginador <maginador@gmail.com>
2022-03-24 13:09:21 -0300

    adjustments for versel

--- api/cryptovoxels.js
+++ api/cryptovoxels.js
@@ -20,6 +20,11 @@ router.get("/", async (req, res) => {
       status: 200,
       message: result,
     });
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
   } catch (error) {
     console.error(error);
     return res.status(500).send("Server error");
--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 const cryptovoxels = require("./api/cryptovoxels");
 const decentraland = require("./api/decentraland");
+const cors = require('cors')
 
 app.use(express.json({ extended: false }));
-
+app.use(cors());
 app.use("/cryptovoxel/", cryptovoxels);
 app.use("/cryptovoxel/", decentraland);
 
-const PORT = process.env.PORT || 6060;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
