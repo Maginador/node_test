7629a1815e565f7696d4bb325d48f261dac02225
Maginador <maginador@gmail.com>
2022-03-23 21:56:43 -0300

    adjustments for versel

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ app.use(express.json({ extended: false }));
 app.use("/cryptovoxel/", cryptovoxels);
 app.use("/cryptovoxel/", decentraland);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 6060;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
